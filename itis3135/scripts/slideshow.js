24508e5131d1ab194d4a0eb943bbce09eb6b1468
cjgalla <collinjosephgallagher05@gmail.com>
2025-04-26 16:50:26 -0400

    Update slideshow.js

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,17 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
-document.querySelector(".next").addEventListener("click", () => plusSlides(1));
-
-document.querySelectorAll(".image-link").forEach(link => {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
-        let num = parseInt(this.getAttribute("data-slide"));
-        currentSlide(num);
-    });
-});
-
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
@@ -21,11 +10,19 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let slides = document.getElementsByClassName("slide");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";  
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
 }
